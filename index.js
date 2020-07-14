faeda4ca114fa077e65afc09275d751c6e1cafde
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-07-14 13:05:26 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+function generate() {
+    const data = {
+        start: $("#from").val(),
+        to: $("#to").val(),
+        base: $("#base").val()
+    };
+
+    if (data.start > data.to) {
+        alert("Starting number may not be greater than ending number!");
+        location.reload();
+        return;
+    }
+    const nums = [];
+    const allowed = [2, 8, 10, 16];
+    if (!allowed.includes(parseInt(data.base))) {
+        alert("Invalid base!");
+        location.reload();
+        return;
+    }
+
+    for (let i = data.start; i <= data.to; i++) {
+        nums.push(parseInt(i).toString(parseInt(data.base)));
+    } 
+
+    let top = `<div class="table-responsive">
+        <table class="table table-bordered table-dark table-hover table-stripped">
+            <thead>
+                <tr>
+                <th scope="col">S.N.</th>
+                <th scope="col">Numbers</th>
+                </tr>
+            </thead>
+            <tbody>
+            `
+    let down = `
+            </tbody>
+        <table>
+    </div>
+    `;
+    nums.forEach((n, i) => {
+        top += `<tr>
+            <th scope="row">${i+1}.</th>
+            <td>${n.toUpperCase()}</td>
+        </tr>`
+    })
+    $(".req").html(top + down);
+}
+
+document.getElementById("bton").addEventListener("click", (event) => {
+    event.preventDefault();
+});
